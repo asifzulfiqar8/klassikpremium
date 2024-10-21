34679e7f855d99b17d18dba98b83125ecbc3aa76
moizxox <mianmoiz899@gmail.com>
2024-10-21 11:23:27 +0500

    New Pages added

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,21 @@ toggleButtons.forEach(function (button) {
     }
   });
 });
-// document.addEventListener("DOMContentLoaded", function() {
+// Accordion Script
+document.addEventListener("DOMContentLoaded", () => {
+  const togglers = document.querySelectorAll("[data-toggle]");
 
-// });
+  togglers.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const selector = e.currentTarget.dataset.toggle;
+      const block = document.querySelector(`${selector}`);
+      if (e.currentTarget.classList.contains("active")) {
+        block.style.maxHeight = "";
+      } else {
+        block.style.maxHeight = block.scrollHeight + "px";
+      }
+
+      e.currentTarget.classList.toggle("active");
+    });
+  });
+});
