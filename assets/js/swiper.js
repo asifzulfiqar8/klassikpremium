50133a1fbc34917d30fd381a4ea06431b680a924
arxllannn <147667804+arxllannn@users.noreply.github.com>
2024-10-29 18:53:06 +0500

    carousels fucntional now

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,121 +1,146 @@
 // Swiper JS Script
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  keyboard: {
-    enabled: true,
-  },
-  pagination: {
-    el: ".swiper-pagination-hero",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".hero-next",
-    prevEl: ".hero-prev",
-  },
-});
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: ".swiper-pagination-hero",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
+  //   For Product Section
+  var productSwiper1 = new Swiper(".proodSwiper1", {
+    slidesPerView: 4, // Display 4 products at a time
+    spaceBetween: 30, // Space between the products
+    loop: true,
+    navigation: {
+      nextEl: ".product-carousel__carousel-1 .prod-next",
+      prevEl: ".product-carousel__carousel-1 .swiper-button-prev",
+    },
+    pagination: {
+      // el: ".swiper-pagination",
+      clickable: false,
+    },
+    breakpoints: {
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      950:{
+          slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      550: {  // Match 786px as the breakpoint
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      0: {  // At 0px or below
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+    },
+  });
+  var productSwiper2 = new Swiper(".proodSwiper2", {
+    slidesPerView: 4, // Display 4 products at a time
+    spaceBetween: 30, // Space between the products
+    loop: true,
+    navigation: {
+      nextEl: ".product-carousel__carousel-2 .prod-next",
+      prevEl: ".product-carousel__carousel-2 .prod-prev",
+    },
+    pagination: {
+      // el: ".swiper-pagination",
+      clickable: false,
+    },
+    breakpoints: {
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      950:{
+          slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      550: {  // Match 786px as the breakpoint
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      0: {  // At 0px or below
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+    },
+  });
+  
 
-//   For Icon Section 1
-var iconSwiper = new Swiper(".iconSwiper", {
-  slidesPerView: 4, // Display 4 products at a time
-  spaceBetween: 30, // Space between the products
-  loop: true, // Infinite loop
-  navigation: {
-    nextEl: ".swp-icon-next",
-    prevEl: ".swp-icon-prev",
-  },
-  pagination: {
-    // el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    1280: {
-      slidesPerView: 4,
-      spaceBetween: 30,
+  var productSwiper3 = new Swiper(".proodSwiper3", {
+    slidesPerView: 4, // Display 4 products at a time
+    spaceBetween: 30, // Space between the products
+    loop: true,
+    navigation: {
+      nextEl: ".product-carousel__carousel-3 .prod-next",
+      prevEl: ".product-carousel__carousel-3 .swiper-button-prev",
     },
-    710: {
-      slidesPerView: 3,
-      spaceBetween: 30,
+    pagination: {
+      // el: ".swiper-pagination",
+      clickable: false,
     },
-    420: {
-      // Match 786px as the breakpoint
-      slidesPerView: 2,
-      spaceBetween: 30,
+    breakpoints: {
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      950:{
+          slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      550: {  // Match 786px as the breakpoint
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      0: {  // At 0px or below
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
     },
-    0: {
-      // At 0px or below
-      slidesPerView: 1,
-      spaceBetween: 10,
+  });
+  //   For Icon Section
+  var iconSwiper = new Swiper(".iconSwiper", {
+    slidesPerView: 4, // Display 4 products at a time
+    spaceBetween: 30, // Space between the products
+    loop: true, // Infinite loop
+    navigation: {
+      nextEl: ".swp-icon-next",
+      prevEl: ".swp-icon-prev",
     },
-  },
-});
-//   For Icon Section 2
-var iconSwiper = new Swiper(".iconSwiper2", {
-  slidesPerView: 4, // Display 4 products at a time
-  spaceBetween: 30, // Space between the products
-  loop: true, // Infinite loop
-  navigation: {
-    nextEl: ".iconSwiper2 .swp-icon-next",
-    prevEl: ".iconSwiper2 .swp-icon-prev",
-  },
-  pagination: {
-    // el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    1280: {
-      slidesPerView: 4,
-      spaceBetween: 30,
+    pagination: {
+      // el: ".swiper-pagination",
+      clickable: true,
     },
-    710: {
-      slidesPerView: 3,
-      spaceBetween: 30,
+    breakpoints: {
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      710:{
+          slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      420: {  // Match 786px as the breakpoint
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      0: {  // At 0px or below
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
     },
-    420: {
-      // Match 786px as the breakpoint
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    0: {
-      // At 0px or below
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-  },
-});
-
-//   For Product Section
-var productSwiper = new Swiper(".productSwiper", {
-  slidesPerView: 4, // Display 4 products at a time
-  spaceBetween: 30, // Space between the products
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    // el: ".swiper-pagination",
-    clickable: false,
-  },
-  breakpoints: {
-    1280: {
-      slidesPerView: 4,
-      spaceBetween: 30,
-    },
-    950: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-    550: {
-      // Match 786px as the breakpoint
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    0: {
-      // At 0px or below
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-  },
-});
+  });
