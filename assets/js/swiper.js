f5f64d41f6664e7362a7e71c437356f204f33dab
arxllannn <147667804+arxllannn@users.noreply.github.com>
2024-10-30 10:06:02 +0500

    code optimized and issue resolve 3 carousel 1 hero or 2 icons swiper

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -11,8 +11,8 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".hero-next",
+      prevEl: ".hero-prev",
     },
   });
   
@@ -23,7 +23,7 @@ var swiper = new Swiper(".mySwiper", {
     loop: true,
     navigation: {
       nextEl: ".product-carousel__carousel-1 .prod-next",
-      prevEl: ".product-carousel__carousel-1 .swiper-button-prev",
+      prevEl: ".product-carousel__carousel-1 .prod-prev",
     },
     pagination: {
       // el: ".swiper-pagination",
@@ -120,6 +120,38 @@ var swiper = new Swiper(".mySwiper", {
     navigation: {
       nextEl: ".swp-icon-next",
       prevEl: ".swp-icon-prev",
+    },
+    pagination: {
+      // el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      710:{
+          slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      420: {  // Match 786px as the breakpoint
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      0: {  // At 0px or below
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+    },
+  });
+  //  icon section 2 
+  var iconSwiper = new Swiper(".iconSwiper2", {
+    slidesPerView: 4, // Display 4 products at a time
+    spaceBetween: 30, // Space between the products
+    loop: true, // Infinite loop
+    navigation: {
+      nextEl: ".iconSwiper2 .swp-icon-next",
+      prevEl: ".iconSwiper2 .swp-icon-prev",
     },
     pagination: {
       // el: ".swiper-pagination",
